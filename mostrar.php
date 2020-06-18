21dcb3feb30911db4908489afa713b4c00ec1568
Marco Antonio Mtz <marcoamtz99@gmail.com>
2020-06-18 16:30:28 -0500

    Update mostrar.php

--- mostrar.php
+++ mostrar.php
@@ -6,7 +6,7 @@ echo <<<EOT
 <html>
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="/css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
 	<title>API</title>
 </head>
 <body>
