2d7b28c18637ed4e92d6763d1f929e3ff10c1ffb
Marco Antonio Mtz <marcoamtz99@gmail.com>
2020-06-20 02:17:52 -0500

    Update crear.php

--- crear.php
+++ crear.php
@@ -93,6 +93,29 @@
 
 				</div>
 					<?php 
+					$lat_1 = $_POST["id"];
+					$lat_2 = $_POST["modelo"];
+						$lat_3 = $_POST["marca"];
+							$lat_4 = $_POST["color"];
+								$lat_5 = $_POST["largo"];
+									$lat_6 = $_POST["ancho"];
+										$lat_7 = $_POST["peso"];
+											$lat_8 = $_POST["tipo"];
+												$lat_9 = $_POST["numCuerdas"];
+													$lat_10 = $_POST["imagen"];
+
+				   $postData = array(
+				   '$id' => $lat_1 ,
+				   'marca' =>$lat_3 ,
+				   'modelo' => $lat_2,
+				   'color' => $lat_4 ,
+				   'peso' => $lat_7,
+				   'largo' => $lat_5 ,
+				   'ancho' => $lat_6,
+				   'numCuerdas' => $lat_9,
+				   'imagen' => $lat_10
+				   ); 
+
 
 					if(isset($_POST['Enviar'])){
 					/* 	$id = $_GET['id']; */
@@ -103,29 +126,7 @@
 	 					$datos = json_decode($json,true); */
 
 						echo "Estoy guardadndo los datos dentro";
-						  $lat_1 = $_POST["id"];
-							 $lat_2 = $_POST["modelo"];
-	 							$lat_3 = $_POST["marca"];
-	 								$lat_4 = $_POST["color"];
-										 $lat_5 = $_POST["largo"];
-	 										$lat_6 = $_POST["ancho"];
-												 $lat_7 = $_POST["peso"];
-													 $lat_8 = $_POST["tipo"];
-	 													$lat_9 = $_POST["numCuerdas"];
-	 														$lat_10 = $_POST["imagen"];
-
-							$postData = array(
-							'$id' => $lat_1 ,
-							'marca' =>$lat_3 ,
-							'modelo' => $lat_2,
-							'color' => $lat_4 ,
-							'peso' => $lat_7,
-							'largo' => $lat_5 ,
-							'ancho' => $lat_6,
-							'numCuerdas' => $lat_9,
-							'imagen' => $lat_10
-							); 
-
+						  
 
 							
 						
