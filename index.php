6335b6cf6b9de391de6ad7c9fec19c96b7050926
Marco Antonio Mtz <marcoamtz99@gmail.com>
2020-06-19 21:40:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -57,12 +57,53 @@
 			<div class="center">
 				<section id="content">
 					<h2 class="subheader">Ultimas guitarras</h2>
+					<div id="articles">
 
 
+					<?php   
+						$url = "https://thawing-caverns-82463.herokuapp.com/guitarras/" ;
+							$json = file_get_contents($url);
+								 $datos = json_decode($json,true);
+								 $lat_1 = $datos["id"];
+								 $lat_2 = $datos["modelo"];
+								 $lat_3 = $datos["marca"];
+								 $lat_4 = $datos["color"];
+								 $lat_5 = $datos["largo"];
+								 $lat_6 = $datos["ancho"];
+								 $lat_7 = $datos["peso"];
+								 $lat_8 = $datos["tipo"];
+								 $lat_9 = $datos["numCuerdas"];
+								 $lat_10 = $datos["imagen"];
+							
+								 
+									   echo	"ID: ".$lat_1  ; 
+									   echo "<br>";
+									   echo	"modelo: ".$lat_2  ;
+									   echo "<br>";
+									   echo	"marca: ".$lat_3  ;
+									   echo "<br>";
+									   echo	"color: ".$lat_4  ;
+									   echo "<br>";
+									   echo	"largo: ".$lat_5  ;
+									   echo "<br>";
+									   echo	"ancho: ".$lat_6  ;
+									   echo "<br>";
+									   echo	"peso: ".$lat_7  ;
+									   echo "<br>";
+									   echo	"tipo: ".$lat_8  ;
+									   echo "<br>";
+									   echo	"numCuerdas: ".$lat_9  ;
+									   echo "<br>";
+									  /*  echo	"imagen: ".$lat_10  ; */
+									   echo "<br>";		   
+					
+					?>
+
+					</div>
 							
 
 						<!-- Listado de guitarras-->
-						<!-- 	<div id="articles">
+						<!-- 	
 						
 						
 						<article class="article-item" id="article-template">
@@ -76,7 +117,7 @@
 							<a href="#">Leer mas</a>
 							<div class="clearfix"></div>
 						</article>	
-						</div> -->
+						 -->
 						
 						
 						
